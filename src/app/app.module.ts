0ff8d5936fc5f77c4ad08d056504c8d95de45484
Waqar Ali <waqar.ali911@gmail.com>
2016-10-12 12:28:44 +0400

    Added basic components and styling, all static.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
